ef745b5edd801f91416cab5279dc5737b4f7ca0e
Mark IJbema <markijbema@gmail.com>
2011-10-03 23:04:03 +0200

    added setup.py so we can pip install this

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-headcrumbs',
+    version = '0.1',
+    description = 'Smart and easy-to-use breadcrumbs for Django',
+    #author = '',
+    #author_email = '',
+    url = 'https://github.com/markijbema/django-headcrumbs',
+    license = 'MIT',
+    packages = find_packages(),
+)
